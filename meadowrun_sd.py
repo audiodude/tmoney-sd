9879b63112f7147cae29dc8ff3823c10f3d63d7a
Travis Briggs <audiodude@gmail.com>
2022-09-14 18:37:40 -0700

    Formatting

--- meadowrun_sd.py
+++ meadowrun_sd.py
@@ -4,10 +4,12 @@ import uuid
 
 import meadowrun
 
+
 def get_prompt():
   if len(sys.argv) != 2:
     raise ValueError('usage: meadowrun_sd "<prompt>"')
   return sys.argv[1]
+
 
 def main():
   folder_name = str(uuid.uuid4())
@@ -20,10 +22,9 @@ def main():
           '--include sd-v1-4.ckpt '
           f'&& python scripts/txt2img.py --prompt "{prompt}" --plms '
           '--ckpt /var/meadowrun/machine_cache/sd-v1-4.ckpt --outdir /tmp/outputs '
-          f' && echo "{prompt}" >> /tmp/outputs/prompt.txt '
+          f'&& echo "{prompt}" >> /tmp/outputs/prompt.txt '
           f'&& aws s3 sync /tmp/outputs s3://tmoney-sd/{folder_name} '
-          '\'',
-          meadowrun.AllocCloudInstance("EC2"),
+          '\'', meadowrun.AllocCloudInstance("EC2"),
           meadowrun.Resources(logical_cpu=1,
                               memory_gb=8,
                               max_eviction_rate=80,
