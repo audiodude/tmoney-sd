2cca622b86f3093b9368a6de7cddae9dc95b6b5a
Travis Briggs <audiodude@gmail.com>
2022-09-14 19:47:26 -0700

    First iteration of web gallery

--- tmoney-sd/web/app.py
+++ tmoney-sd/web/app.py
@@ -1,24 +1,31 @@
 import boto3
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/")
 def index():
   s3 = boto3.client('s3')
+  s3_resource = boto3.resource('s3')
   resp = s3.list_objects(Bucket='tmoney-sd', Delimiter='/')
   all_prefixes = []
   for obj in resp.get('CommonPrefixes'):
     all_prefixes.append(obj.get('Prefix'))
 
-  urls = []
+  galleries = []
   for prefix in all_prefixes:
-    response = s3.generate_presigned_url('get_object',
-                                         Params={
-                                             'Bucket': 'tmoney-sd',
-                                             'Key': f'{prefix}grid-0000.png'
-                                         },
-                                         ExpiresIn=3600)
-    urls.append(response)
-  return urls
+    url = s3.generate_presigned_url('get_object',
+                                    Params={
+                                        'Bucket': 'tmoney-sd',
+                                        'Key': f'{prefix}grid-0000.png'
+                                    },
+                                    ExpiresIn=3600)
+
+    prompt_key = f'{prefix}prompt.txt'
+    obj = s3_resource.Object('tmoney-sd', prompt_key)
+    prompt = obj.get()['Body'].read().decode('utf-8').strip()
+
+    galleries.append({'url': url, 'prompt': prompt})
+
+  return flask.render_template('index.html.jinja2', galleries=galleries)
